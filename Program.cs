5f49cffe6da95c47a15f52d6ea1ea71d02b0eb94
Senfernandes <senfernandes@gmail.com>
2019-03-18 22:50:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Google.Cloud.Vision.V1;   
+using System;
+
+namespace CloudVisionWithCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = ImageAnnotatorClient.Create();
+            
+            var image = Image.FromFile("images/003.jpg");
+            
+            var response = client.DetectLabels(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine($"Score: {annotation.Score} Description: {annotation.Description}");
+            }
+        }
+    }
+}
